01dc35f9a16fdbdd9ef4266ab61d237b2b2da554
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-05-03 17:46:23 +0900

    인트로영역 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from "react"
+import dongseok from "../assets/img/dongseok.jpg";
+
+const introText = {
+  title: "port developer" ,
+  desc: ["tlaent is", "found at the end of the", "effort" ],
+
+};
 
 const Intro = () => {
   return (
-    <div>
-      Intro
-    </div>
+    <section id="intro">
+            <div className="intro_inner">
+                <h2 className="intro_title">
+                    {introText.title}
+                </h2>
+                <div className="intro_lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="intro_text">
+                    <div className="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
+                    </div>
+                    <div className="img">
+                        <img src={dongseok} alt="어바웃미"/>
+                    </div>
+                </div>
+                <div className="intro_lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
   )
 }
 

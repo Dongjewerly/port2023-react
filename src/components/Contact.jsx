cddd54aa32ff5ed41091ac9753c238600df2ce79
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-05-14 16:32:41 +0900

    연락처완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React from "react";
+const contactText = [
+    {
+        link:"https://blog.naver.com/dongseog_you/223434116836",
+        title:"KAKAO : DongSeok, Yu",
+    },
+    {
+        link:"https://blog.naver.com/dongseog_you/223434116836",
+        title:"NAVER : dongseog_you@naver.com",
+    }
+]; 
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <section id="contact">
+            <div className="contact_inner">
+                <h2 className="contact_title">Contact</h2>
+                <div className="contact_lines top" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="contact_text">
+                    <div className="text">
+                        {contactText.map((contact,key)=>(
+                            <div key={key}>
+                                <a 
+                                href={contact.link}
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                >
+                                    {contact.title}
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="contact_lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
   )
 }
 
